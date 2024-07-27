8d58da8bb72e8ad42ccc87eee0c33453689af2d9
aditya bhojane <adityabhojane66@gmail.com>
2024-07-27 09:58:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,31 @@ import Accordion from "./Components/Accordion"
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
+  const toggle = (e) => {
+    const accordionItem = e.currentTarget;
+    const allAccordions = document.querySelectorAll('.accordion-item');
+ 
+    allAccordions.forEach((item) => {
+        if(item !== accordionItem){
+          item.style.height = "60px"
+        }
+        else if(accordionItem.style.height == "120px"){
+          item.style.height = "60px"
+        }
+        else{
+          item.style.height = "120px"
+        }
+    });
+  }
 
   return (
     <>
-      <div className="w-[800px] h-[500px] border border-white rounded-xl">
-        <Accordion  question="Best Way to solve doubts in pw skills ?" answer="It's Super Fast Email Support"></Accordion>
-        <Accordion  question="What is the best thing in pw skills ?" answer="It's Management"></Accordion>
-        <Accordion  question="How's the pw skills platform (website) ?" answer="Fast login, Video player is great"></Accordion>
-        <Accordion  question="Doubt session in pw skills ?" answer="Doubt Mentor is very good specially in DSA"></Accordion>
+      <div className="w-[800px] h-[320px] border border-white rounded-xl">
+        <Accordion toggle={toggle}  question="Best Way to solve doubts in pw skills ?" answer="It's Super Fast Email Support"></Accordion>
+        <Accordion toggle={toggle}  question="What is the best thing in pw skills ?" answer="It's Management"></Accordion>
+        <Accordion toggle={toggle}  question="How's the pw skills platform (website) ?" answer="Fast login, Video player is great"></Accordion>
+        <Accordion toggle={toggle}  question="Doubt session in pw skills  ?" answer="Doubt Mentor is very good specially in DSA"></Accordion>
       </div>
     </>
   )
--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 
-function Accordion({question, answer}) {
-  let answerOpen = false;
-  function ShowAnswer(e){
-    const parent = e.currentTarget;
-      if(answerOpen){
-        parent.style.height = "55px"
-        answerOpen = false;
-      }else{
-        parent.style.height = "120px";
-        answerOpen = true
-      }
-  }
+function Accordion({question, answer, toggle}) {
+
   return (
     <>
-    <div onClick={ShowAnswer} className="outerParent w-full h-[60px] overflow-hidden transition-all">
+    <div onClick={toggle} style={{height:"60px"}} className="accordion-item w-full overflow-hidden transition-all">
       <h1 className="p-4 bg-blue-900 rounded-xl text-[25px] cursor-pointer select-none">
           {question}
       </h1>
-      <div  className= {` bg-blue-950 w-full  text-[20px] `}>
+      
+      <div  className= "bg-blue-950 w-full  text-[20px]">
         <p className='p-4'>{answer}</p> 
       </div>
     </div>
@@ -27,3 +18,5 @@ function Accordion({question, answer}) {
 }
 
 export default Accordion;
+
+
